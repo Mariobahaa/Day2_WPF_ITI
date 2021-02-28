c3cb1b3288ffc1ed3991a43325682e0826d5e526
MarioBahaa <32718005+Mariobahaa@users.noreply.github.com>
2021-02-28 16:02:17 +0200

    Calculator

---
+++ P1/Calculator.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace P1
+{
+    /// <summary>
+    /// Interaction logic for Calculator.xaml
+    /// </summary>
+    public partial class Calculator : Window
+    {
+        bool SymbPressed = false;
+        float Prev = 0;
+        String prevSymb = "=";
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var digit = (e.Source as Button).Content.ToString();
+
+            if (Result.Text=="0" || SymbPressed)
+            {
+                Result.Text = digit ;
+            }
+            else
+            {
+                Result.Text += digit;
+            }
+            SymbPressed = false;
+        }
+
+        private void Button_Click_op(object sender, RoutedEventArgs e)
+        {
+            var symbol = prevSymb;
+            prevSymb = (e.Source as Button).Content.ToString();
+
+            switch (symbol)
+            {
+                case ".":
+                    Result.Text += symbol;
+                    Point.IsEnabled = false;
+                    break;
+                case "+":
+                    Prev = float.Parse(Result.Text.ToString()) + Prev;
+                    Result.Text = Prev.ToString();
+                    Point.IsEnabled = true;
+                    SymbPressed = true;
+                    break;
+                case "/":
+                    Prev = float.Parse(Result.Text.ToString()) / Prev;
+                    Result.Text = Prev.ToString();
+                    Point.IsEnabled = true;
+                    SymbPressed = true;
+                    break;
+                case "x":
+                    Prev = float.Parse(Result.Text.ToString()) * Prev;
+                    Result.Text = Prev.ToString();
+                    Point.IsEnabled = true;
+                    SymbPressed = true;
+                    break;
+                case "-":
+                    Prev = float.Parse(Result.Text.ToString()) - Prev;
+                    Result.Text = Prev.ToString();
+                    Point.IsEnabled = true;
+                    SymbPressed = true;
+                    break;
+                case "=":
+                    Prev = float.Parse(Result.Text.ToString());
+                    Result.Text = Prev.ToString();
+                    Point.IsEnabled = true;
+                    SymbPressed = true;
+                    break;
+            }
+
+        }
+    }
+}
